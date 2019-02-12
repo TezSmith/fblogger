fb72e36b243af65dbcba91b30679b06cb8659eb4
Tez Smith <montez.smith@flatironschool.com>
2019-02-12 09:34:46 -0500

    Started adding Redux

---
+++ src/actions/actiontypes.js
@@ -0,0 +1,2 @@
+export const THIS_ACTION = "THIS_ACTION"
+export const THAT_ACTION = "THAT_ACTION"
--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer'
+
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
