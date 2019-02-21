aeebee40ffdf36e951ec5f0c9d8b3dc7ff90ccfc
Tez Smith <montez.smith@flatironschool.com>
2019-02-21 09:22:08 -0500

    Added Login Toggle

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,8 +12,24 @@ export function toggleLogin() {
 //Register Action
 export function register(data) {
   return dispatch => {
-    console.log("I'm in the register action:", data)
-  }
+fetch('http://localhost:3000/users', {
+  method: 'POST',
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify(data)
+}).then(res => res.json())
+  .then(res => {
+      // Sets the token in local storage
+      window.localStorage.setItem('jwt', res.jwt)
+      let jwt = window.localStorage.getItem('jwt')
+      let result = jwtDecode(jwt)
+
+      // In the next then, push user to the next page in your app.
+      // Also you'll need to decode the JWT token in order to get the user data has
+      // For that npm install jwt-decode
+      // Then you'll need to dispatch an action that updates your user key in the initial store
+      dispatch({type: REGISTER_ACTION, payload: {id: result.id, first_name: result.first_name} })
+  })
+ }
 }
 
 //Login Function
