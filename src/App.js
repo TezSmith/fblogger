638a09d9ba1649288162c5f73aedaa3c808ad94e
TezSmith <Montez.Smith1@gmail.com>
2019-04-08 21:52:59 -0400

    added toml file

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-             <p>Testing</p>
              <AccountCont/>
              <Route path="/callback" component={Callback}/>
              <Route path="/profile" component={Profile}/>
